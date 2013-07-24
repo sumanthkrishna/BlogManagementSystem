115b9811e1ded0931144591c17d2d3c666b8e6df
THINKPAD <nani@ubuntu.(none)>
2013-07-24 21:57:20 +0530

    blog created

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6b7c3e4ebdbba14792f4fb6c82d26955f026f66949eed723c22e834b88ebf1028902adb49923af1c41501eef00b780428796368dc87710c12ab3d43472525dc8'
